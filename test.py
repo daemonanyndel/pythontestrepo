9949c38aac20b398491c693917d8b79f6170e93b
daemonanyndel <romangonzalo88@gmail.com>
2021-03-10 10:07:41 +0100

    Testing multiply and print

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from main import add
+from main import multiply
 
 
 def test_add():
@@ -6,5 +7,14 @@ def test_add():
     num2 = 2
     results = num1 + num2
     assert results == 5
+    print("Add was okay")
 
+def test_multiply():
+    num1 = 2
+    num2 = 3
+    results = num1 * num2
+    assert results == 6
+    print("Multiply was okay")
+    
 test_add()
+test_multiply()
