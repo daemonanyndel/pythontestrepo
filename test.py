ca07daa9d9727d0b9eeda4bf5d8ac74c63476cf5
daemonanyndel <romangonzalo88@gmail.com>
2021-03-10 10:33:50 +0100

    Testing wether test_add() is needed

--- test.py
+++ test.py
@@ -6,15 +6,15 @@ def test_add():
     num1 = 3
     num2 = 2
     results = num1 + num2
-    assert results == 5
+    assert results == 4
     print("Add was okay")
 
 def test_multiply():
     num1 = 2
     num2 = 3
     results = num1 * num2
-    assert results == 6
+    assert results == 5
     print("Multiply was okay")
     
-test_add()
-test_multiply()
+#test_add()
+#test_multiply()
